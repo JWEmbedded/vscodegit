ef5145354ed387430f3d786bb88a1609a62a14fc
Jeongwoo Yeom <studavid@naver.com>
2024-12-14 23:15:14 +0900

    make dfs

--- namseok.c
+++ namseok.c
@@ -4,9 +4,15 @@ void still(){
   printf("It is still difficult\n");
 }
 
+void dfs(int now){
+  if(now == 3) return;
+  dfs(now+1);
+}
+
 int main(){
   printf("what is your lunch?\n");
   printf("my favorite is seolyoon\n");
   still();
+  dfs(0);
   return 0;
 }
