2a38ef50138fedc74a8ebe82bf86e1b044d06350
DESKTOP-IFHO09K\YEOM <studavid@naver.com>
2024-12-14 15:51:29 +0900

    i make func

--- namseok.c
+++ namseok.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+void still(){
+  printf("It is still difficult\n");
+}
+
 int main(){
   printf("what is your lunch?\n");
-  printf("my favorite is seolyoon");
+  printf("my favorite is seolyoon\n");
+  still();
   return 0;
 }
